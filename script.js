0b6d933f71837e6d48844a061b361cf1b4c263e6
elise1365 <up2109470@myport.ac.uk>
2024-06-27 11:08:16 +0100

    got the books in one line

--- script.js
+++ script.js
@@ -20,30 +20,42 @@ function drawBooks(listOfBooks) {
         let randNum = Math.floor(Math.random() * 4) +1;
         // alert(randNum);
         let className;
+        let title = listOfBooks[i];
         if(randNum == 1){
             className = "bookSpine1"
+            if(listOfBooks[i].length > 15){
+                title = title.slice(0,15);
+                title += "...";
+            }
         }
         else if(randNum == 2){
             className = "bookSpine2"
+            if(listOfBooks[i].length > 30){
+                title = title.slice(0,30);
+                title += "...";
+            }
         }
         else if(randNum == 3){
             className = "bookSpine3"
+            if(listOfBooks[i].length > 10){
+                title = title.slice(0,10);
+                title += "...";
+            }
         }
-        else if(randNum == 4){
+        else{
             className = "bookSpine4"
+            if(listOfBooks[i].length > 25){
+                title = title.slice(0,25);
+                title += "...";
+            }
         }
 
         const bookSpine = document.createElement("div");
         bookSpine.className = className + " bookSpine";
-
-        let title = listOfBooks[i];
-        if(listOfBooks[i].length > 12){
-            title = title.slice(0,12);
-        }
 
         bookSpine.innerText = title;
         bookShelf.appendChild(bookSpine);
     }
 }
 
-window.onload = drawBooks(["crow road", "as i walked out one midsummer morning"]);
+window.onload = drawBooks(["The Crow Road", "as i walked out one midsummer morning", "klara and the sun", "dfdfd", "cdsfdssd", "The Crow Road", "as i walked out one midsummer morning", "klara and the sun", "dfdfd", "cdsfdssd", "The Crow Road", "as i walked out one midsummer morning", "klara and the sun", "dfdfd", "cdsfdssd", "The Crow Road", "as i walked out one midsummer morning", "klara and the sun", "dfdfd", "cdsfdssd", "The Crow Road", "as i walked out one midsummer morning", "klara and the sun", "dfdfd", "cdsfdssd", "The Crow Road", "as i walked out one midsummer morning", "klara and the sun", "dfdfd", "cdsfdssd", "The Crow Road", "as i walked out one midsummer morning", "klara and the sun", "dfdfd", "cdsfdssd", "The Crow Road", "as i walked out one midsummer morning", "klara and the sun", "dfdfd", "cdsfdssd"]);
