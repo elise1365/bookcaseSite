85bfecc23c89b97e2a723d4ab51c51dab669a8e7
elise1365 <up2109470@myport.ac.uk>
2024-07-01 11:32:52 +0100

    sign up now works on button click!

--- signInUpScript.js
+++ signInUpScript.js
@@ -3,9 +3,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
+let auth;
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+function initApp(){
+    // Your web app's Firebase configuration
+  const firebaseConfig = {
     apiKey: "AIzaSyBJ2lJaYojtl0Gqi6C2yQyA-X_O-PbIBYU",
     authDomain: "bookcasesite-f9ce9.firebaseapp.com",
     projectId: "bookcasesite-f9ce9",
@@ -13,26 +15,28 @@ const firebaseConfig = {
     messagingSenderId: "1028674706274",
     appId: "1:1028674706274:web:c9f2f9be0fbaefb94fea3b",
     measurementId: "G-J7K3W3C2PT"
-};
-  
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+  };
 
-function function1(){
-    // const email = document.getElementById("email").value;
-    // const password = document.getElementById("password").value;
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  return auth;
+};
+
+async function function1() {
+  try {
+    auth = initApp();
     let email = "elise@gmail.com";
     let password = "123WER";
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      alert('User signed up:', userCredential.user);
-    })
-    .catch((error) => {
-      alert(error.code, error.message);
-    });
-};
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    console.log("User created:", userCredential.user);
+  } catch (error) {
+    console.error("Error code:", error.code);
+    console.error("Error message:", error.message);
+    alert(error.message);
+  }
+}
 
-window.onload(function1());
+// window.onload(function1());
 
-// export function1;
+document.getElementById("signUpBttn").addEventListener("click", function1);
