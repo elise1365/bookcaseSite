d3601758053775753ab9850c63a20531749bd73c
Elise Read <up2109470@myport.ac.uk>
2024-08-12 15:01:17 +0100

    added stats

--- mainPage/goalScript.js
+++ mainPage/goalScript.js
@@ -38,7 +38,7 @@ async function getInfoFromDb(){
             year = allData.year;
             goal = allData.goal;
             booksRead = allData.booksRead;
-            displayAllInfo()
+            displayAllInfo();
         })
     }
     else{
@@ -47,10 +47,92 @@ async function getInfoFromDb(){
     }
 }
 
-function displayAllInfo(){
+async function displayAllInfo(){
     // add the title
     let titleElement = document.getElementById("title");
     titleElement.innerHTML = year + " Reading Goal";
+
+    // add bookRead/goal
+    let goalElement = document.getElementById("goal");
+    goalElement.innerHTML = booksRead + " / " + goal + " books read";
+
+    // add stats
+    // percentage complete
+    let percentageElement = document.getElementById("percentage");
+    let percentageCompleted = goal / booksRead;
+    percentageCompleted = Math.floor(percentageCompleted);
+    percentageElement.innerHTML = "- " + percentageCompleted + "% complete"
+
+    // on track / behind / ahead
+    let booksPerMonth = goal / 12;
+
+    // ToDo: what if 2 books r finished on the same day?
+    // ToDo: make sure most recent book is from that correct year
+
+    const db = initApp();    
+    const goalCollection = collection(db, "books");
+    const q = query(goalCollection, where("userID", "==", userID));
+    const snapshot = await getDocs(q);
+
+    let mostRecentDate;
+    let mostRecentBookID;
+    let date;
+
+    if(!snapshot.empty){
+        snapshot.forEach((doc) => {
+            const bookData = doc.data();
+            const bookDate = new Date(bookData.dateFinished);
+
+            if(!mostRecentDate || bookDate > mostRecentDate){
+                mostRecentDate = bookDate;
+                mostRecentBookID = doc.id;
+                date = bookData.dateFinished;
+
+                // get month of most recently read book, x this by booksPerMonth and compare to booksRead
+                let month = date[5] + date[6];
+                let expected = Math.ceil(month * booksPerMonth);
+
+                let onTrack;
+
+                if(expected >= goal){
+                    onTrack = "You are on track to achieve your goal";
+                }
+                else{
+                    onTrack = "You are behind on your reading goal";
+                }
+
+                let trackElement = document.getElementById("track");
+                trackElement.innerHTML = onTrack;
+            }
+        }
+    );
+    } else{
+        // ToDo: error stuff!
+        console.log("no book found for this user")
+        return null;
+    }
+
+    // how often user needs to finish a book to achieve goal
+    let planElement = document.getElementById("plan");
+    planElement.innerHTML = "In order to reach your goal, you need to read " + Math.ceil(booksPerMonth) + " books per month - ";
+}
+
+function editMode(){
+    // convert edit bttn to save bttn
+    let editBttn = document.getElementById("editBttn");
+    editBttn.innerHTML = "check";
+    editBttn.id = "saveBttn";
+    editBttn.removeEventListener("click", editMode);
+    editBttn.addEventListener("click", saveChanges);
+}
+
+function saveChanges(){
+    // convert save bttn back to edit bttn
+    let saveBttn = document.getElementById("saveBttn");
+    saveBttn.innerHTML = "edit";
+    saveBttn.id = "editBttn";
+    saveBttn.removeEventListener("click", saveChanges);
+    saveBttn.addEventListener("click", editMode);
 }
 
 window.onload = getInfoFromDb();
@@ -60,6 +142,13 @@ document.addEventListener("DOMContentLoaded", () => {
     if(backBttn){
         backBttn.addEventListener("click", () => {
             window.location.href = "../mainPage/bookcase.html";
+        })
+    }
+
+    let editBttn = document.getElementById("editBttn");
+    if(editBttn){
+        editBttn.addEventListener("click", () => {
+            editMode();
         })
     }
 });
